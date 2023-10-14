04be6bbcaccaf4df410fedcdf317b1f7e492ccc4
oli-lane <markus.oliver.lane@gmail.com>
2023-10-14 15:22:04 -0700

    functions no applications or testing

--- asign3.rkt
+++ asign3.rkt
@@ -7,15 +7,22 @@
 ; - Lab 3 code
 ; - ifleq0?
 ; - Binop (+ - * /)
-; - Functions
+; - Functions and ids
 
-; data definitions
+; Abstract Syntax Tree definitions
 (struct NumC([n : Real]) #:transparent)
 (struct BinopC([op : Symbol] [l : ExprC] [r : ExprC]) #:transparent)
+; ifleq0? x : y else: z
 (struct Cond0C([test : ExprC] [then : ExprC] [else : ExprC]) #:transparent)
-(define-type ExprC (U NumC BinopC Cond0C))
+(struct IdC([s : Symbol]) #:transparent)
+(struct AppC([fun : Symbol] [arg : ExprC]) #:transparent)
+(define-type ExprC (U NumC BinopC Cond0C IdC AppC))
+
+; functions
+(struct FundefC([name : Symbol] [arg : Symbol] [body : ExprC]) #:transparent)
 
 
+; ***** Parser *****
 ; recursively map Sexp to ExprC
 (define (parse [s : Sexp]) : ExprC
   (match s
@@ -27,8 +34,45 @@
     [(list 'ifleq0? x ': y 'else: z) (Cond0C (parse x) (parse y) (parse z))]
     [other (error 'parse "PAIG: expected legal expression, got ~e" other)]))
 
+; given a PAIG function parse into a FundefC
+(define (parse-fundef [s : Sexp]) : FundefC
+  (match s
+    ; destructure fundef, create a FunDefC, parse body
+    [(list 'fun (list name (list arg)) body) (FundefC name arg (parse body))]
+    ; better error check here?
+    [other (error 'parse-fundef "PAIG: expected legal function definition, got ~e" other)]))
+
+; given a list of PAIG functions, parse into a list of FundefC
+(define (parse-prog [s : Sexp]) : (Listof FundefC)
+  (match s
+    ['() '()]
+    ; add more formatting here? enforce syntax?
+    ; input is list of functions, so f is a function, r is a list of functions.
+    ; use parse-fundef to parse a function, and then recursively call parse-prog
+    ; to parse the rest of the functions
+    [(cons f r) (cons (parse-fundef f) (parse-prog r))]
+    ; better error check here?
+    [other (error 'parse-prog "PAIG: expected legal program, got ~e" other)]
+    ))
+
+; ***** Interpreter *****
+
+(: top-interp (Sexp -> Real))
+(define (top-interp fun-sexps)
+  (interp-fns (parse-prog fun-sexps)))
+
+; interprets the function named main from function definitions
+(define (interp-fns [funs : (Listof FundefC)]) : Real
+  (match funs
+    ; no main function
+    ['() (error 'interp-fns "PAIG: couldn't find \"main\" function")]
+    ; found main function
+    [(cons (FundefC 'main arg body) r) (interp body funs)]
+    ; check next function in list
+    [(cons f r) (interp-fns r)]))
+
 ; recursively interpret ExprCs
-(define (interp [e : ExprC]) : Real
+(define (interp [e : ExprC] [funs : (Listof FundefC)]) : Real
   (match e
     [(NumC n) n]
     ; use binop-lookup to assign meaning to binop
@@ -40,6 +84,7 @@
       [else (interp z)])]))
 
 ; match binary operator to meaning
+; from asgn spec, should this return the function ex: (+)?
 (define (binop-lookup [b : BinopC]) : Real
   (match b
     [(BinopC '+ l r) (+ (interp l) (interp r))]
@@ -49,6 +94,10 @@
     [other (error 'binop-lookup "PAIG: expected valid binop, got ~e" other)]))
 
 
+; errors to check for
+; - don't let other functions call main
+; - check for taken ids
+; - check for poorly created functions
 
 
 
