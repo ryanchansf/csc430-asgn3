0ad11c311a4a1b334a083c0b199a2010ceeeb9db
oli-lane <markus.oliver.lane@gmail.com>
2023-10-28 16:30:06 -0700

    4 type check errors

--- asign5.rkt
+++ asign5.rkt
@@ -5,13 +5,13 @@
 
 
 ; new features
-; - change abstract syntax
+; @- change abstract syntax
 ; - update parser and interpreter to handle new abstract syntax
-; - add environments
-; - serialize
-; - booleans
-; - closures (straight out of book)
-; - desugar with to blam
+; @- add environments
+; @- serialize
+; @- booleans
+; @- closures (straight out of book)
+; @- desugar with to blam
 ; - user error raising
 ; - built-ins (equal, <=, *, -, etc)
 
@@ -59,20 +59,72 @@
 (struct BoolV([val : Boolean]) #:transparent)
 (struct NumV([val : Real]) #:transparent)
 (struct StrV([val : String]) #:transparent)
-(struct CloV([arg : Symbol] [body : ExprC] [env : Env]) #:transparent)
-(struct PrimV([val : (-> ExprC * ExprC)]) #:transparent)
-
+(struct CloV([args : (Listof IdC)] [body : ExprC] [env : Env]) #:transparent)
+(struct PrimV([val : (-> Value Value Value)]) #:transparent)
 
 (define-type Value (U NumV StrV BoolV CloV PrimV))
 
+
 (define top-env (cons (Binding 'true (BoolV #t))
                       (cons (Binding 'false (BoolV #f))
-                            (cons (Binding '+ (PrimV +))
-                                  (cons (Binding '- (PrimV -))
-                                        (cons (Binding '* (PrimV *))
-                                              (cons (Binding '/ (PrimV /))
-                                                    (cons (Binding '<= (PrimV <=))
-                                                          (cons (Binding 'equal? (PrimV equal?)) '())))))))))
+                            (cons (Binding '+ (PrimV top-plus))
+                                  (cons (Binding '- (PrimV top-minus))
+                                        (cons (Binding '* (PrimV top-mult))
+                                              (cons (Binding '/ (PrimV top-divide))
+                                                    (cons (Binding '<= (PrimV top-<=))
+                                                          (cons (Binding 'equal? (PrimV top-equal?)) '())))))))))
+; given two values, add them together or error if illegal
+(define (top-plus [l : Value] [r : Value]) : NumV
+  (cond
+    [(and (NumV? l) (NumV? r)) (NumV (+ (NumV-val l) (NumV-val r)))]
+    [else (error '+ "PAIG: non-number operands to (+ a b) → real")]))
+
+; given two values, subtract them or error if illegal
+(define (top-minus [l : Value] [r : Value]) : NumV
+  (cond
+    [(and (NumV? l) (NumV? r)) (NumV (- (NumV-val l) (NumV-val r)))]
+    [else (error '- "PAIG: non-number operands to (- a b) → real")]))
+
+; given two values, multiply them together or error if illegal
+(define (top-mult [l : Value] [r : Value]) : NumV
+  (cond
+    [(and (NumV? l) (NumV? r)) (NumV (* (NumV-val l) (NumV-val r)))]
+    [else (error '* "PAIG: non-number operands to (* a b) → real")]))
+
+; given two values, divide them or error if illegal
+(define (top-divide [l : Value] [r : Value]) : NumV
+  (cond
+    [(and (NumV? l) (NumV? r)) (cond
+                                 [(equal? (NumV-val r) 0) (error '/ "PAIG: illegal divide by 0")]
+                                 [else (NumV (/ (NumV-val l) (NumV-val r)))])]
+    [else (error '/ "PAIG: non-number operands to (/ a b) → real")]))
+
+; given two values l and r, return l <= r or error if illegal
+(define (top-<= [l : Value] [r : Value]) : BoolV
+  (cond
+    [(and (NumV? l) (NumV? r)) (BoolV (<= (NumV-val l) (NumV-val r)))]
+    [else (error '<= "PAIG: non-number operands to (<= a b) → boolean")]))
+
+; give two values l and r, return l == r or error if illegal
+(define (top-equal? [l : Value] [r : Value]) : BoolV
+  (cond
+    [(and (NumV? l) (NumV? r)) (BoolV (equal? (NumV-val l) (NumV-val r)))]
+    [(and (StrV? l) (StrV? r)) (BoolV (equal? (StrV-val l) (StrV-val r)))]
+    [(and (BoolV? l) (BoolV? r)) (BoolV (equal? (BoolV-val l) (BoolV-val r)))]
+    [else (BoolV #f)]))
+
+; ***** Serializer *****
+
+; given a PAIG5 Value, return a string
+(define (serialize [val : Value]) : String
+  (match val
+    [(NumV n) (format "~v" n)]
+    [(StrV s) (format "~v" s)]
+    [(BoolV b) (cond
+                 [(equal? b #t) "true"]
+                 [else "false"])]
+    [(CloV a b e) "#<procedure>"]
+    [(PrimV p) "#<primop>"]))
 
 
 ; ***** Parser *****
@@ -93,19 +145,19 @@
     ; desugar with to AppC and BlamC
     [(list 'with (list (? list? locals) ...) ': body) (AppC (BlamC (map desugar-id locals) (parse body)) (map desugar-expr locals))]
     ; parse function applications to AppC
-    [(cons f (? list? r))] ;"TODO"
+    [(cons f (? list? r)) (AppC (parse f) (map parse r))]
     ; catch illegal expressions
     [other (error 'parse "PAIG: expected legal expression, got ~e" other)]))
 
 ; desugar with local expr to ExprC
-(define (desugar-expr [locals : (Listof Sexp)] [body : Sexp]) : ExprC
-  (match
+(define (desugar-expr [local : (Listof Sexp)]) : ExprC
+  (match local
     [(list expr 'as _) (parse expr)]
     [other (error 'desugar-expr "PAIG: expected valid with clause, got ~e" other)]))
 
 ; desugar with local id to IdC
-(define (desugar-id [local : (Listof Sexp)] [body : Sexp]) : IdC
-  (match
+(define (desugar-id [local : (Listof Sexp)]) : IdC
+  (match local
     [(list _ 'as id) (parse-id id)]
     [other (error 'desugar-id "PAIG: expected valid with clause, got ~e" other)]))
 
@@ -123,91 +175,55 @@
     ; catch illegal ids
     [else (error 'parse-id "PAIG: expected legal id, got ~e" s)]))
 
-; given a symbol, check if it's a valid binary operator
-(define (check-binop [s : Symbol]) : Symbol
-  (cond
-    [(equal? s '-) s]
-    [(equal? s '+) s]
-    [(equal? s '*) s]
-    [(equal? s '/) s]
-    [else (error 'check-binop "PAIG: expected legal binary operator, got ~e" s)]))
-
-; given a PAIG function definition, create a corresponding FundefC
-(define (parse-fundef [s : Sexp]) : FundefC
-  (match s
-    ; ensure main has no arguments
-    [(list 'fun (list 'main args) body) (cond
-                                          [(equal? args '()) (FundefC (IdC 'main) '() (parse body))]
-                                          [else (error 'parse-fundef
-                                                       "PAIG: expected no arguments to main, got ~e" args)])]
-    ; parse PAIG function definition
-    [(list 'fun (list name (list (? symbol? args) ...)) body)
-     (cond
-       [(equal? #f (check-duplicates (cast args (Listof Symbol))))
-        (FundefC (parse-id name) (map parse-id (cast args (Listof Symbol))) (parse body))]
-       [else (error 'parse-fundef "PAIG: no duplicate arguments, got ~e" args)])]
-    ; catch illegal function definitions
-    [other (error 'parse-fundef "PAIG: expected legal function definition, got ~e" other)]))
-
-; given a list of PAIG functions (a PAIG program), parse into a list of FundefC
-(define (parse-prog [s : Sexp]) : (Listof FundefC)
-  (match s
-    ['() '()]
-    [(cons f r) (cons (parse-fundef f) (parse-prog r))]
-    ; catch invalid program structure
-    [other (error 'parse-prog "PAIG: expected legal program, got ~e" other)]))
-
 
 ; ***** Interpreter *****
 
-; given an Sexp, combine parsing and evaluation to run program
-(: top-interp (Sexp -> Real))
-(define (top-interp fun-sexps)
-  (interp-fns (parse-prog fun-sexps)))
-
-; given the list of functions from a program, interprets the function named main from function definitions
-(define (interp-fns [funs : (Listof FundefC)]) : Real
-  ; evaluate program by applying main function to argument 0
-  (interp (AppC (IdC 'main) '()) funs))
+; given an Sexp, combine parse and evaluate, serialize final Value
+(define (top-interp [s : Sexp]) : String
+  (serialize (interp (parse s) top-env)))
 
 ; given an ExprC and list of FundefCs, recursively evaluate ExprCs to resolve applications
 (define (interp [e : ExprC] [env : Env]) : Value
   (match e
-    [(NumC n) n]
+    [(NumC n) (NumV n)]
+    [(StrC s) (StrV s)]
     ; any Id not substituted is unbound
-    [(IdC s) (lookup s env)] 
-    ; use binop-lookup to assign meaning to binop
-    [(BinopC s l r) (binop-lookup (BinopC s l r) funs)]
-    ; check if x <= 0
-    [(Cond0C x y z) (cond
-                      [(<= (interp x funs) 0) (interp y funs)]
-                      [else (interp z funs)])]
+    [(IdC s) (lookup (IdC s) env)]
+    ; evaluate conditional
+    [(CondC c t e) (local ([define c-val (interp c env)])
+                     (cond
+                       [(equal? c-val (BoolV #t)) (interp t env)]
+                       [(equal? c-val (BoolV #f)) (interp e env)]
+                       [else (error 'interp "PAIG: expected boolean value from condition, got ~e" c-val)]))]
+    ; evalute BlamC
+    [(BlamC args body) (CloV args body env)]
     ; find function, eagerly evaluate arguments, then evaluate body
-    [(AppC f vals) (cond
-                     [(equal? (length vals) (length (BlamC-args f))) (interp f
-                           (extend-env (map (λ (arg val) (Binding arg (interp val env))) (BlamC-args f) vals) mt-env))]
-                     [else (error
-                              'interp "PAIG: Incorrect number for arguments for function: \"~e\""
-                              f)])]))
+    ; interp f into CloV, extend env based on current
+    [(AppC f vals) (local ([define f-value (interp f env)])
+                     (cond
+                       ; anonymous function application
+                       [(CloV? f-value) (cond
+                                          [(equal? (length vals) (length (CloV-args f-value)))
+                                           (interp (CloV-body f-value)
+                                                   (extend-env (map (λ ([arg : Symbol] [val : ExprC]) : Binding (Binding arg (interp val env))) (CloV-args f-value) vals) env))]
+                                          [else (error
+                                                 'interp "PAIG: Incorrect number of arguments for function: \"~e\""
+                                                 f-value)])]
+                       ; built-in function application
+                       [(PrimV? f-value) (cond
+                                           [(equal? (length vals) 2) ((PrimV-val f-value) (interp (first vals) env) (interp (second vals) env))]
+                                           [else (error 'interp "PAIG: Incorrect number of arguments, expected 2, got ~e" (length vals))])]
+                       [else (error 'interp "PAIG: illegal function application, cannot apply ~e" f-value)]))]))
+
 
 ; lookup binding in environment
 (define (lookup [s : IdC] [env : Env]) : Value
   (match env
     ['() (error 'lookup "PAIG: name not found: ~e" s)]
     [(cons (Binding name val) r) (cond
-                                   [(symbol=? s name) val]
+                                   [(symbol=? (IdC-s s) name) val]
                                    [else (lookup s r)])]))
 
 
-; given a valid BinopC and a list of FundefCs, match BinopC's binary operator to its meaning and evaluate
-(define (binop-lookup [b : BinopC] [funs : (Listof FundefC)]) : Real
-  (match b
-    [(BinopC '+ l r) (+ (interp l funs) (interp r funs))]
-    [(BinopC '* l r) (* (interp l funs) (interp r funs)) ]
-    [(BinopC '- l r) (- (interp l funs) (interp r funs)) ]
-    [(BinopC '/ l r) (local ([define divisor (interp r funs)])
-                       (cond
-                         [(equal? 0 divisor) (error 'binop-lookup "PAIG: cannot divide by 0")]
-                         [else (/ (interp l funs) divisor)]))]))
 
 
